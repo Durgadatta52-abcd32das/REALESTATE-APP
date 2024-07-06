f7e2ab6406fc64be7eeb4936bee648d0d800f1f1
Your Name <durgadattadas52@gmail.com>
2024-07-06 06:59:16 -0700

    First Commit

---
+++ realestate/client/src/firebase.js
@@ -0,0 +1,23 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDjCbUQ_pyrfjiah23GordZLf11PGB6EcM",
+
+  authDomain: "real-estate-mern-6ab06.firebaseapp.com",
+
+  projectId: "real-estate-mern-6ab06",
+
+  storageBucket: "real-estate-mern-6ab06.appspot.com",
+
+  messagingSenderId: "204605541920",
+
+  appId: "1:204605541920:web:7156a1249517c42c7ff645"
+
+};
+
+
+// Initialize Firebase
+
+export const app = initializeApp(firebaseConfig);
